ff806ae93c0c2e4fd5c8d84e6f0c95b47fa55e4a
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2025-04-16 14:02:18 +0200

    ADD Tiny forest RPG tileset CHANGE code BasicPlayerController ADD Tilemap ADD Player Collision ADD Tilemap Collision

--- Assets/Scripts/BaseCharacterController.cs
+++ Assets/Scripts/BaseCharacterController.cs
@@ -7,6 +7,13 @@ public class BaseCharacterController : MonoBehaviour
 {
     private Vector2 movementInput;
     [SerializeField] float movementSpeed;
+    [Range(0,1)][SerializeField] float slowedFactor;
+    private bool isSlowed;
+
+    private void Start()
+    {
+        isSlowed = false;
+    }
 
     /// <summary>
     /// Movement is called by the input system when the player moves the joystick or presses the arrow keys.
@@ -18,9 +25,29 @@ public class BaseCharacterController : MonoBehaviour
         movementInput = ctx.ReadValue<Vector2>(); //comment
     }
 
-    private void Update()
+    //This is now a FIXEDupdate
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed;
+        //var actualMovementSpeed = isSlowed ? movementSpeed * slowedFactor : movementSpeed;
+        var actualMovementSpeed = movementSpeed;
+        if(isSlowed) actualMovementSpeed *= slowedFactor;
+
+        transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * actualMovementSpeed);
     }
 
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Swamp"))
+        {
+            isSlowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Swamp"))
+        {
+            isSlowed = false;
+        }
+    }
 }
