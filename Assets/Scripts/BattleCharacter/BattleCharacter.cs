d7d16c3286be814fd632ef5c04c38ccfd94b3e3f
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2025-05-08 08:46:09 +0200

    EDIT BattleCharacters.cs

--- Assets/Scripts/BattleCharacter/BattleCharacter.cs
+++ Assets/Scripts/BattleCharacter/BattleCharacter.cs
@@ -11,7 +11,10 @@ public class BattleCharacter: MonoBehaviour
     //private Dictionary<string, Abbility> Abilities;
     private GameObject playerPrefab;
 
-    public virtual void LoadPlayerPrefab() { }
+    public virtual void LoadPlayerPrefab() 
+    {
+        Instantiate();
+    }
     public virtual void Attack(BattleCharacter target) { }
     public virtual void Defend(BattleCharacter target) { }
     public virtual void UseAbility(BattleCharacter target, string abilityName) { }
