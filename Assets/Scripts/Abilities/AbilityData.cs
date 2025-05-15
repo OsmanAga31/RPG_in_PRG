c45f5235e019ed86363287417b4b1399491db04e
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2025-05-15 12:58:15 +0200

    ADD Abilities

---
+++ Assets/Scripts/Abilities/AbilityData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAbilityData", menuName = "Battle/Ability")]
+public class AbilityData : ScriptableObject
+{
+    public string abilityName;
+    public AbilityTargetType target;
+    public AbilityType type;
+    public AbilityStatType stat;
+    public uint abilityTime;
+}
+
+public enum AbilityTargetType
+{
+    SingleTarget,
+    MultiTarget,
+}
+
+public enum AbilityType
+{
+    incremental,
+    decremental,
+}
+
+public enum AbilityStatType
+{
+    health,
+    maxHealth,
+    attack,
+    defense,
+    revival,
+}
